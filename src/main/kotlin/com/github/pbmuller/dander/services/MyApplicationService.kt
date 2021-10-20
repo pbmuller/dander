40ddb0903fcc39546f828c14b2f6d6f18742a666
GitHub Action <action@github.com>
2021-10-20 17:01:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/pbmuller/dander/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pbmuller/dander/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pbmuller.dander.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pbmuller.dander.MyBundle
 
 class MyApplicationService {
 
